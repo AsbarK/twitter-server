8d442c9548df309e2e12f106ba5ec793e17dad68
asbar <asbar03KK@gmail.com>
2023-12-30 01:07:22 +0530

    implimented the query to get urls for image

--- src/app/tweet/queries.ts
+++ src/app/tweet/queries.ts
@@ -2,4 +2,5 @@
 
 export const queries = `#graphql
     GetAllTweets:[Tweet]
+    GetSignedUrls(imageType:String!): String
 `
--- src/app/tweet/resolvers.ts
+++ src/app/tweet/resolvers.ts
@@ -1,11 +1,19 @@
 import { Tweet } from "@prisma/client"
 import { prismaClient } from "../../client/db"
 import { GraphqlContext } from "../../interfaces"
+import {S3Client,PutObjectCommand} from "@aws-sdk/client-s3"
+import {getSignedUrl} from '@aws-sdk/s3-request-presigner'
+
+require('dotenv').config()
 
 interface CreateTweetData{
     content: string
     imageUrl?: string
 }
+const s3Client = new S3Client({
+    region: 'ap-south-1',
+    credentials:{accessKeyId:String(process.env.S3_ACCESS_KEY),secretAccessKey:String(process.env.S3_ACCESS_SECRET)}
+})
 
 const mutations = {
     CreateTweet:async(parent:any,{payload}:{payload:CreateTweetData},ctx:GraphqlContext)=>{
@@ -22,7 +30,19 @@ const mutations = {
 }
 const queries = {
     GetAllTweets:async(parent:any,args:any,ctx:GraphqlContext)=>{
-        return await prismaClient.tweet.findMany()
+        return await prismaClient.tweet.findMany({orderBy:{createdAt:'desc'}})
+    },
+    GetSignedUrls:async (parent:any,{imageType}:{imageType:String},ctx:GraphqlContext) => {
+        if(!ctx.user || !ctx.user.id) throw new Error('UnAuthorized')
+
+        const putObjectCommand = new PutObjectCommand({
+            Bucket: 'asbar-twitter-dev',
+            Key: `uploads/${ctx.user.id}/tweets/${Date.now().toString()}.${imageType}`,
+            
+        })
+
+        const signedUrl = await getSignedUrl(s3Client,putObjectCommand)
+        return signedUrl
     }
 }
 
